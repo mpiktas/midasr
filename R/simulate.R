993c2f5cae0aceda1f90c0666ffa93a7446fb089
Vaidotas Zemlys <zemlys@gmail.com>
2012-03-15 06:18:16 +0200

    Added the documentation for the first two functions

--- R/simulate.R
+++ R/simulate.R
@@ -1,15 +1,16 @@
 ##' Simulate AR(1) or MA(1) model
 ##'
 ##' Simulate MIDAS regressor as a AR(1) or MA(1) time series
-##' @title 
+##'  
 ##' @param model A named vector of length one. Name is either "ar", or "ma"
 ##' depending on which AR(1) or MA(1) process should be generated
-##' @param n length of output series
+##' @param n the length of output series
 ##' @param innov.sd the standard error of innovations, which are zero mean normal random variables
 ##' @param frequency the frequency of the regressor, should larger than one.
-##' @param n.start length of the burn.in period, the default is 300.
-##' @return a time-series object of class "ts" 
+##' @param n.start the length of the burn.in period, the default is 300.
+##' @return a time-series object of class \code{ts} 
 ##' @author Vaidotas Zemlys
+##' @export
 simplearma.sim <- function(model,n,innov.sd,frequency,n.start=300) {
     e <- rnorm(n+n.start,sd=innov.sd)
     if(names(model)=="ar"){
@@ -18,21 +19,22 @@ simplearma.sim <- function(model,n,innov.sd,frequency,n.start=300) {
     if(names(model)=="ma"){
         res <- c(0,e[2:length(e)]+model*e[1:(length(e)-1)])
     }
-    res[-n.start:-1]
+    ts(res[-n.start:-1],frequency=frequency)
 }
 
 ##' Simulate MIDAS regresion response variable
 ##'
 ##' Given the predictor variable and the coefficients calculate MIDAS regression response variable.
-##'
 ##' It is assumed that predictor variable has long enough history.
-##' @title 
-##' @param n 
-##' @param theta 
-##' @param x 
-##' @param eps.sd 
-##' @return 
+##' 
+##' @param n The sample size
+##' @param theta a vector with MIDAS regression parameters 
+##' @param x a \code{ts} object with MIDAS regression predictor variable
+##' @param eps.sd the standard error of the regression disturbances, which are assumed to be independent normal zero mean random variables 
+##' @return a \code{ts} object
 ##' @author Vaidotas Zemlys
+##' @export
+##' @import foreach
 midas.sim <- function(n,theta,x,eps.sd) {
     m <- frequency(x)
     n.x <- length(x)
