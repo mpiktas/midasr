677468d0fb22cb43b810ddb9ec2da7e2625b9cbf
Vaidotas Zemlys <zemlys@gmail.com>
2012-03-28 23:37:22 +0300

    Added examples to simulation functions

--- R/simulate.R
+++ R/simulate.R
@@ -6,10 +6,13 @@
 ##' depending on which AR(1) or MA(1) process should be generated
 ##' @param n the length of output series
 ##' @param innov.sd the standard error of innovations, which are zero mean normal random variables
-##' @param frequency the frequency of the regressor, should larger than one.
+##' @param frequency the frequency of the regressor, should be larger than one.
 ##' @param n.start the length of the burn.in period, the default is 300.
 ##' @return a time-series object of class \code{ts} 
-##' @author Vaidotas Zemlys
+##' @author Vaidotas Zemlys (zemlys@gmail.com)
+##' @examples
+##' #Generate AR(1) model with rho=0.6, with frequency 12
+##' x <- simplearma.sim(list(ar=0.6),1500*12,1,12)
 ##' @export
 simplearma.sim <- function(model,n,innov.sd,frequency,n.start=300) {
     e <- rnorm(n+n.start,sd=innov.sd)
@@ -28,11 +31,31 @@ simplearma.sim <- function(model,n,innov.sd,frequency,n.start=300) {
 ##' It is assumed that predictor variable has long enough history.
 ##' 
 ##' @param n The sample size
-##' @param theta a vector with MIDAS regression parameters 
+##' @param theta a vector with MIDAS regression coefficients 
 ##' @param x a \code{ts} object with MIDAS regression predictor variable
 ##' @param eps.sd the standard error of the regression disturbances, which are assumed to be independent normal zero mean random variables 
 ##' @return a \code{ts} object
-##' @author Vaidotas Zemlys
+##' @author Vaidotas Zemlys (zemlys@gmail.com)
+##' @examples
+##' ##The parameter function
+##' theta.h0 <- function(p, dk) {
+##'    i <- (1:dk-1)/100
+##'    pol <- p[3]*i + p[4]*i^2
+##'    (p[1] + p[2]*i)*exp(pol)
+##' }
+##'
+##' ##Generate coefficients
+##' theta0 <- theta.h0(c(-0.1,10,-10,-10),4*12)
+##'
+##' ##Plot the coefficients
+##' plot(theta0)
+##'
+##' ##Generate the predictor variable
+##' x <- simplearma.sim(list(ar=0.6),1500*12,1,12)
+##'
+##' ##Simulate the response variable
+##' y <- midas.sim(n,theta0,x,1)
+##' 
 ##' @export
 ##' @import foreach
 midas.sim <- function(n,theta,x,eps.sd) {
