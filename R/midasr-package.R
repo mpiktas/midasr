d49dd3b088e2cf8de5ebfef1f8fc9b4467ea774b
Vaidotas Zemlys <zemlys@gmail.com>
2012-04-11 11:11:14 +0300

    Bump version to 0.0.3. Add package documentation. Add US unemployment data.

---
+++ R/midasr-package.R
@@ -0,0 +1,23 @@
+##' Package for estimating and testing MIDAS regression.
+##' 
+##' The main feature of this package is function \code{\link{hAh.test}}
+##' which performs test whether coefficients of MIDAS regression have certain functional form
+##' 
+##' @name midasr-package
+##' @aliases midasr
+##' @docType package
+##' @title One sentence summary of your package.
+##' @author Virmantas Kvedaras \email{virmantas.kvedaras@@mif.vu.lt}, Vaidotas Zemlys (maintainer) \email{zemlys@@gmail.com}
+##' @keywords package
+NULL
+
+##' US monthly unemployment rate
+##'
+##' The monthly unemployment rate for United States from 1948 to 2011.
+##' 
+##' @name USunempr
+##' @docType data
+##' @format A \code{\link{ts}} object.
+##' @source \href{http://data.bls.gov/timeseries/LNS14000000}{U.S. Bureau of Labor Statistics}
+##' @keywords datasets
+NULL
