22d2ff244e242470095c52eb516212313cf0f935
Vaidotas Zemlys <zemlys@gmail.com>
2012-05-14 16:17:27 +0300

    Fix bug with incorrect S3 method

--- R/fit.R
+++ R/fit.R
@@ -379,15 +379,16 @@ midas_r.fit <- function(x) {
 ##' @param ... further arguments to function \code{\link{grad}} from package \code{numDeriv}
 ##' @return numeric vector
 ##' @author Vaidotas Zemlys
-##' @seealso midas_r.formula 
+##' @seealso midas_r.formula
+##' @import numDeriv
 ##' @rdname gradient
 ##' @export
-gradient <- function(x,...)UseMethod("mdslag")
+gradient <- function(x,...)UseMethod("gradient")
 
 #' @rdname gradient
 #' @method gradient midas_r
 #' @export
-gradient.midas_r <- function(x) {
+gradient.midas_r <- function(x,...) {
     grad(x$fn0,coef(x),...)
 }
 
@@ -402,15 +403,42 @@ gradient.midas_r <- function(x) {
 ##' @return numeric vector
 ##' @author Vaidotas Zemlys
 ##' @rdname hessian 
-##' @seealso midas_r.formula
+##' @seealso midas_r
+##' @import numDeriv
 ##' @export
 hessian <- function(x,...)UseMethod("hessian")
 
 #' @rdname hessian
 #' @method hessian midas_r
+#' @import numDeriv
 #' @export
 hessian.midas_r <- function(x,...) {
-    hessian(x$fn0,coef(x),...)
+   numDeriv::hessian(x$fn0,coef(x),...)
+}
+
+##' Check whether non-linear least squares restricted MIDAS regression problem has converged
+##'
+##' Computes the gradient and hessian of the optimisation function of restricted MIDAS regression and checks whether the conditions of local optimum are met. Numerical estimates are used.
+##' @param x \code{\link{midas_r}} object
+##' @param tol a tolerance, values below the tolerance are considered zero
+##' @return a list with gradient, hessian of optimisation function and convergence message
+##' @rdname check_convergence
+##' @seealso midas_r
+##' @export
+##' @author Vaidotas Zemlys
+deriv_tests<- function(x,tol=1e-6) UseMethod("deriv_tests")
+
+#' @rdname check_convergence
+#' @method check_convergence midas_r
+#' @export
+deriv_tests.midas_r <- function(x,tol=1e-6) {
+    gr <- gradient(x)
+    hess <- hessian(x)
+    egh <- eigen(hess)$values
+
+    first <- sum(abs(gr))<tol
+    second <- any(!egh>0) & any(abs(egh)<tol)
+    list(first=first,second=second,gradient=gr,eigenval=egh)
 }
 
 ##' Return the coefficients of MIDAS regression
