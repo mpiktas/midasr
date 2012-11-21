f97ffcf379650faa6e58575d81e9c9d2bc1a8a84
Vaidotas Zemlys <zemlys@gmail.com>
2012-11-21 17:09:00 +0200

    Move method code to a new file. Add methods vcov, bread, estfun. Move code for derivatives to a new file

---
+++ R/deriv.R
@@ -0,0 +1,144 @@
+##' Check whether non-linear least squares restricted MIDAS regression problem has converged
+##'
+##' Computes the gradient and hessian of the optimisation function of restricted MIDAS regression and checks whether the conditions of local optimum are met. Numerical estimates are used.
+##' @param x \code{\link{midas_r}} object
+##' @param tol a tolerance, values below the tolerance are considered zero
+##' @return a list with gradient, hessian of optimisation function and convergence message
+##' @rdname deriv_tests
+##' @seealso midas_r
+##' @export
+##' @author Vaidotas Zemlys
+deriv_tests<- function(x,tol=1e-6) UseMethod("deriv_tests")
+
+#' @rdname deriv_tests
+#' @method deriv_tests midas_r
+#' @export
+deriv_tests.midas_r <- function(x,tol=1e-6) {
+    gr <- gradient(x)
+    hess <- hessian(x)
+    egh <- eigen(hess)$values
+
+    first <- sum(abs(gr))<tol
+    second <- !any(egh<0) & !any(abs(egh)<tol)
+    list(first=first,second=second,gradient=gr,eigenval=egh)
+}
+
+##' Calculate numerical approximation of gradient of RSS of
+##' MIDAS regression 
+##'
+##' Calculate nummerical approximation of gradient of residual sum of
+##' squares function of estimated MIDAS regression
+##' 
+##' @param x a \code{\link{midas_r}} object
+##' @param ... further arguments to function \code{\link{grad}} from package \code{numDeriv}
+##' @return numeric vector
+##' @author Vaidotas Zemlys
+##' @seealso midas_r.formula
+##' @import numDeriv
+##' @rdname gradient
+##' @export
+gradient <- function(x,...)UseMethod("gradient")
+
+#' @rdname gradient
+#' @method gradient midas_r
+#' @export
+gradient.midas_r <- function(x,...) {
+    grad(x$fn0,coef(x),...)
+}
+
+##' Calculate numerical approximation of gradient of RSS of
+##' MIDAS regression 
+##'
+##' Calculate nummerical approximation of gradient of residual sum of
+##' squares function of estimated MIDAS regression
+##' 
+##' @param x a \code{\link{midas_r}} object
+##' @param ... further arguments to function \code{\link{hessian}} from package \code{numDeriv}
+##' @return numeric vector
+##' @author Vaidotas Zemlys
+##' @rdname hessian 
+##' @seealso midas_r
+##' @import numDeriv
+##' @export
+hessian <- function(x,...)UseMethod("hessian")
+
+#' @rdname hessian
+#' @method hessian midas_r
+#' @import numDeriv
+#' @export
+hessian.midas_r <- function(x,...) {
+   numDeriv::hessian(x$fn0,coef(x),...)
+}
+
+##' Gradient function of coefficient function in MIDAS regression
+##'
+##' Returns gradient function of coefficient function
+##' 
+##' @param x \code{\link{midas_r}} object
+##' @param gr a function or a list of functions for each restriction.
+##' @param ... arguments supplied to gradient function(s)
+##' @return a function
+##' @author Vaidotas Zemlys
+##' @details MIDAS regression can be written in the following form:
+##'
+##' \deqn{y=Xf(\eta)+u}
+##'
+##' This function calculates
+##'
+##' \deqn{\frac{\partial f}{\partial \eta}}
+##' 
+gradD <- function(x,gr=NULL,...) {
+    rf <- lapply(x$param.map,function(x)return(function(p,...)p)) 
+    names(rf) <- names(x$param.map)
+
+    restr.name <- names(x$restrictions)   
+    
+    if(is.null(gr)) {
+        rf[names(x$restrictions)] <- x$restrictions
+        all_coef <- function(p) {
+             pp <- lapply(x$param.map,function(xx)p[xx])     
+             res <- mapply(function(fun,param)fun(param),rf,pp,SIMPLIFY=FALSE)
+             unlist(res)
+         }
+
+        gr <- function(p,...)jacobian(all_coef,p)
+    }
+    else {
+        if(is.function(gr) & length(x$restrictions)==1) {
+            grf <- list(gr)
+            names(grf) <- names(x$restrictions)
+        }
+        else {
+            if(is.null(names(gr)) | !is.list(gr)) stop("Argument gr must be a named list")
+            if(any(!names(gr) %in% names(x$restrictions))) stop("The names of the list must coincide with the names of the restriction functions")
+            grf <- lapply(x$param.map,function(x)return(function(p)return(matrix(1))))
+            grf[names(x$restrictions)] <- gr[names(x$restrictions)]
+        }
+        pp0 <- lapply(x$param.map,function(xx)coef(x)[xx])            
+        grmat0 <- mapply(function(fun,param,MoreArgs=...)fun(param,...),grf,pp0,SIMPLIFY=FALSE)
+        colnos <- sapply(grmat0,ncol)
+        rownos <- sapply(grmat0,nrow)
+        np <- length(colnos)
+        ccol <- cumsum(colnos)
+        rrow <- cumsum(rownos)
+        pinds <- cbind(c(1,rrow[-np]+1),rrow,
+                       c(1,ccol[-np]+1),ccol)
+        pinds <- apply(pinds,1,function(x)list(row=x[1]:x[2],col=x[3]:x[4]))
+                
+        gr <- function(p,...) {
+            pp <- lapply(x$param.map,function(x)p[x])
+            grmat <- mapply(function(fun,param,MoreArgs=...)fun(param,...),grf,pp,SIMPLIFY=FALSE)
+            if(length(grmat)==1) {
+                res <- grmat[[1]]
+            }
+            else {
+                res <- matrix(0,nrow=sum(rownos),ncol=sum(colnos))
+                mapply(function(m,ind){res[ind$row,ind$col] <- m},
+                       grmat,pinds)
+                   
+            }
+            res
+        }
+    }
+    function(p)gr(p,...)
+}
---
+++ R/midas_r_methods.R
@@ -0,0 +1,139 @@
+##' MIDAS regression model deviance
+##'
+##' Returns the deviance of a fitted MIDAS regression object
+##' @param object a \code{\link{midas_r}} object
+##' @param ... currently nothing
+##' @return The sum of squared residuals
+##' @author Virmantas Kvedaras.Vaidotas Zemlys
+##' @rdname deviance
+##' @method deviance midas_r
+deviance.midas_r <- function(object,...) {
+    sum(residuals(object)^2,na.rm=TRUE)
+}
+
+##' Predict method for MIDAS regression fit
+##'
+##' Predicted values based on MIDAS regression object
+##' @param object \code{\link{midas_r}} object
+##' @param newldata new low frequency data, must be \code{data.frame}
+##' @param newhdata new high frequency data, must be \code{data.frame}
+##' @param ... additional arguments, not used
+##' @return a vector of predicted values
+##' @author Virmantas Kvedaras, Vaidotas Zemlys
+##' @method predict midas_r
+##' @export
+predict.midas_r <- function(object, newldata, newhdata, ... ) {
+    if(missing(newldata) | missing(newhdata))
+      return(as.vector(fitted(object)))
+
+    if(!is.data.frame(newldata)| !is.data.frame(newhdata)) stop("New data must supplied as data.frame")
+    
+    Zenv <- new.env(parent=parent.frame())
+
+    ee <- as.environment(c(as.list(newldata),as.list(newhdata)))
+    parent.env(ee) <- parent.frame()
+    
+    assign("ee",ee,Zenv)
+    cll <- match.call()
+    mf <- match.call(expand.dots = FALSE)
+    m <- match(c("object", "newldata"), names(mf), 0L)
+    mf <- mf[c(1L, m)]
+    mf[[1L]] <- as.name("model.frame")
+    mf[[2L]] <- formula(object)
+    mf[[3L]] <- as.name("ee")   
+    mf[[4L]] <- as.name("na.omit")
+    names(mf)[c(2,3,4)] <- c("formula","data","na.action")
+    
+    mf <- eval(mf,Zenv)
+    mt <- attr(mf, "terms")
+
+    X <- model.matrix(mt, mf)
+    as.vector(X %*% midas_coef(object))
+}
+
+##' @export
+##' @method summary midas_r
+summary.midas_r <- function(object, ...) {
+    r <- as.vector(residuals(object))
+    param <- coef(object)
+    pnames <- names(param)
+    n <- length(r)
+    p <- length(param)
+    rdf <- n - p
+    resvar <- deviance(object)/rdf
+    XD <- object$model[,-1]%*%gradD(object)(coef(object))
+    R <- qr.R(qr(XD))
+    XDtXDinv <- chol2inv(R)
+    dimnames(XDtXDinv) <- list(pnames,pnames)
+    se <- sqrt(diag(XDtXDinv)*resvar)
+    tval <- param/se
+    param <- cbind(param,se,tval,2*pt(abs(tval),rdf,lower.tail=FALSE))
+    dimnames(param) <- list(pnames, c("Estimate", "Std. Error", 
+        "t value", "Pr(>|t|)"))
+    ans <- list(formula=formula(object$terms),residuals=r,sigma=sqrt(resvar),
+                df=c(p,rdf), cov.unscaled=XDtXDinv, call=object$call,
+                coefficients=param,midas.coefficients=midas_coef(object))
+    class(ans) <- "summary.midas_r"
+    ans
+}
+
+##' @export
+##' @method print summary.midas_r
+print.summary.midas_r <- function(x, digits=max(3, getOption("digits") - 3 ), signif.stars = getOption("show.signif.stars"), ...) {
+    cat("\n Formula", deparse(formula(x)),"\n")
+    df <- x$df
+    rdf <- df[2L]
+    cat("\n Parameters:\n")
+    printCoefmat(x$coefficients,digits=digits,signif.stars=signif.stars,...)
+    cat("\n Residual standard error:", format(signif(x$sigma,digits)), "on", rdf , "degrees of freedom\n")
+    invisible(x)
+}
+
+##' @export
+##' @method print midas_r
+print.midas_r <- function(x, digits=max(3,getOption("digits")-3),...) {
+    cat("MIDAS regression model\n")
+    cat(" model:", deparse(formula(x)),"\n")
+    print(coef(x),digits = digits, ...)
+    cat("\n")
+    cat("Function", x$argmap.opt$Rfunction, "was used for fitting\n")
+    invisible(x)
+}
+
+##' @import sandwich
+##' @export
+##' @method estfun midas_r
+estfun.midas_r <- function(x,...) {
+    XD <- x$model[,-1]%*%gradD(x)(coef(x))
+    rval <- as.vector(residuals(x))*XD
+    colnames(rval) <- names(coef(x))
+    rval
+}
+
+##' @import sandwich
+##' @export
+##' @method bread midas_r
+bread.midas_r <- function(x,...) {
+    sandwich:::bread.nls(x,...)
+}
+
+##' @export
+##' @method vcov midas_r
+vcov.midas_r <- function(object,...) {
+    stats:::vcov.nls(object,...)
+}
+
+
+##' @export
+##' @method logLik midas_r
+logLik.midas_r <- function(object,...) {
+    res <- residuals(object)
+    N <- length(res)
+    val <- -N * (log(2 * pi) + 1 - log(N)  +  log(sum(res^2)))/2
+    attr(val, "df") <- 1L + length(coef(object))
+    attr(val, "nobs") <- attr(val, "nall") <- N
+    class(val) <- "logLik"
+    val
+}
+
+
