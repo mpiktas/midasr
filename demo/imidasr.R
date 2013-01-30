3e30943dcc9776d5681c1c3662e2c4fd13abf868
Vaidotas Zemlys <zemlys@gmail.com>
2013-01-30 15:52:05 +0200

    Fix a typo

--- demo/imidasr.R
+++ demo/imidasr.R
@@ -54,7 +54,7 @@ z <- ts(simplearma.sim(list(ar=0.5),length(y),1,frequency(y)),start=start(y))
 yy <- y+z*0.5+1
 
 ###By hand
-mr2 <- midas_r(yy~fmls(dx,4*12-1,12,pp)+mls(x,4*12,12)+z,start=list(dx=c(-0.1,10,-10,-10)))
+mr2 <- midas_r(yy~fmls(dx,4*12-1,12,pp1)+mls(x,4*12,12)+z,start=list(dx=c(-0.1,10,-10,-10)))
 
 ###With special function
 imr2 <- imidas_r(yy~fmls(x,4*12-1,12,theta.h0)+z,start=list(x=c(-0.1,10,-10,-10)))
