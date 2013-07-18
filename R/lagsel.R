9879551e59a63d7cb615180da19cb6c6bdc11077
Vaidotas Zemlys <zemlys@gmail.com>
2013-07-18 12:45:52 +0300

    Move statistical test code to tests.R Add nobs element to midas_r output, indicating number of low frequency observations Add lagsel function, which selects the number of lags in a model. Currently experimental.

--- R/lagsel.R
+++ R/lagsel.R
@@ -1,3 +1,115 @@
-lagsel <- function(formula,ldata,hdata,kmin,kmax) {
+##' Select lags of MIDAS regression using information criteria
+##'
+##' Select lags of MIDAS regression using information criteria
+##' @param x the formula for MIDAS regression, the lag selection is performed for the first mls term in the formula
+##' @param ldata low frequency data, a \code{data.frame} object
+##' @param hdata high frequency data, a \code{data.frame} object
+##' @param start the starting values for optimisation
+##' @param kmin the minimum high frequency lag, defaults to zero.
+##' @param kmax the highest high frequency lag, defaults to square root of number of low fequency observations
+##' @param IC the information criterias on which to base selection
+##' @param Ofunction see \link{midasr}
+##' @param user.gradient see \link{midas_r}
+##' @param ... additional parameters to optimisation function, see \link{midas_r}
+##' @return a table with 
+##' @author Vaidotas Zemlys
+lagsel <- function(x,ldata=NULL,hdata=NULL,start,kmin=NULL,kmax=NULL,IC=list(AIC,BIC),Ofunction="optim",user.gradient=FALSE,...) {
     
+    Zenv <- new.env(parent=environment(x))
+      
+    if(missing(ldata)|missing(hdata)) {
+        ee <- NULL
+    }
+    else {
+        data <- check_mixfreq(ldata,hdata)
+
+        ee <- as.environment(c(as.list(data$lowfreq),as.list(data$highfreq)))
+        parent.env(ee) <- parent.frame()
+    }
+    assign("ee",ee,Zenv)
+    cl <- match.call()
+    mf <- match.call(expand.dots = FALSE)
+    ##Fix this!!
+    m <- match(c("x", "ldata"), names(mf), 0L)
+    mf <- mf[c(1L, m)]
+    mf[[1L]] <- as.name("model.frame")
+    mf[[3L]] <- as.name("ee")   
+    mf[[4L]] <- as.name("na.omit")
+    names(mf)[c(2,3,4)] <- c("formula","data","na.action")
+
+    mff <- mf
+    
+    mf <- eval(mf,Zenv)
+    mt <- attr(mf, "terms")
+        
+    args <- list(...)
+    y <- model.response(mf, "numeric")
+
+    if(is.null(kmax))kmax <- sqrt(length(y))
+
+    formulas <- lagformula(x,Zenv,kmin=kmin,kmax=kmax)
+    
+    modellist <- lapply(formulas, function(f) {    
+        mff[[2L]] <- f
+        mmf <- eval(mff,Zenv)
+        mmt <- attr(mmf, "terms")
+        y <- model.response(mmf, "numeric")
+        X <- model.matrix(mmt, mmf)
+        list(mt=mmt,y=y,X=X)
+    })
+
+    lrn <- rownames(modellist[[length(modellist)]]$X)
+    modellist <- lapply(modellist,function(mm) {
+        rn <- rownames(mm$X)
+        ind <- match(lrn,rn)
+        mm$y <- mm$y[ind]
+        mm$X <- mm$X[ind,]
+        mm
+    })
+    mrm <- lapply(modellist,function(mm) {
+        res <- prepmidas_r(mm$y,mm$X,mm$mt,Zenv,cl,args,start,Ofunction,user.gradient,NULL,FALSE)
+        class(res) <- "midas_r"
+        res
+    })
+    candlist <- lapply(mrm,midas_r)
+
+    candlist
+}
+
+lagformula <- function(x,Zenv,kmin=NULL,kmax) {
+    last.term <- x[[3]]
+    if(length(last.term)==3) {
+        last.term <- x[[c(3,3)]]
+        ind <- c(3,3)
+    }
+    else {
+        ind <- 3
+    }
+
+    mtype <- as.character(last.term[[1]])
+    if(!(mtype%in%c("fmls","dmls","mls")))stop("The last term in the formula must be a MIDAS lag term")
+
+    if(mtype=="mls") {
+        mkmin <- min(as.numeric(eval(last.term[[3]],Zenv)))
+        if(is.null(kmin)) {
+            kmin <- min(mkmin)
+        }
+    }
+    else {
+        if(is.null(kmin)) kmin <- 0
+        mkmin <- 0
+    }
+    last.term[[1]] <- as.name("mls")
+
+    lags <- lapply(kmin:kmax,function(x)mkmin:x)
+
+    formulas <- vector("list",length(lags))
+    for(i in 1: length(formulas)) {
+        res <- x
+        lt <- last.term
+        lt[[3]] <- lags[[i]]
+        res[[ind]] <- lt
+        formulas[[i]] <- res
+    }
+    formulas    
 }
---
+++ R/tests.R
@@ -0,0 +1,267 @@
+##' Test restrictions on coefficients of MIDAS regression
+##'
+##' Perform a test whether the restriction on MIDAS regression coefficients holds.
+##' 
+##' @param x MIDAS regression model with restricted coefficients, estimated with \code{\link{midas_r}}
+##' @return a \code{htest} object
+##' @author Virmantas Kvedaras, Vaidotas Zemlys
+##' @references Kvedaras V., Zemlys, V. \emph{Testing the functional constraints on parameters in regressions with variables of different frequency} Economics Letters 116 (2012) 250-254
+##' @seealso hAhr.test
+##' @examples
+##' ##The parameter function
+##' theta.h0 <- function(p, dk) {
+##'    i <- (1:dk-1)
+##'    (p[1] + p[2]*i)*exp(p[3]*i + p[4]*i^2)
+##' }
+##'
+##' ##Generate coefficients
+##' theta0 <- theta.h0(c(-0.1,0.1,-0.1,-0.001),4*12)
+##'
+##' ##Plot the coefficients
+##' plot(theta0)
+##'
+##' ##Generate the predictor variable
+##' set.seed(13)
+##' x <- simplearma.sim(list(ar=0.6),1500*12,1,12)
+##'
+##' ##Simulate the response variable
+##' y <- midas.sim(500,theta0,x,1)
+##'
+##' ##Remove unnecessary history of x
+##' x <- window(x,start=start(y))
+##' 
+##' ##Fit restricted model
+##' mr <- midas_r(y~fmls(x,4*12-1,12,theta.h0)-1,data.frame(y=y),data.frame(x=x),start=list(x=c(-0.1,0.1,-0.1,-0.001)))
+##'
+##' ##Perform test (the expected result should be the acceptance of null)
+##'
+##' hAh.test(mr)
+##' 
+##' ##Fit using gradient function
+##'
+##' ##The gradient function
+##' theta.h0.gradient<-function(p, dk) {
+##'    i <- (1:dk-1)
+##'    a <- exp(p[3]*i + p[4]*i^2)
+##'    cbind(a, a*i, a*i*(p[1]+p[2]*i), a*i^2*(p[1]+p[2]*i))
+##' }
+##'
+##' mr <- midas_r(y~fmls(x,4*12-1,12,theta.h0)-1,data.frame(y=y),data.frame(x=x),start=list(x=c(-0.1,0.1,-0.1,-0.001)),user.gradient=TRUE)
+##'
+##' ##The test will use an user supplied gradient of weight function. See the
+##' ##help of midas_r on how to supply the gradient.
+##' 
+##' hAh.test(mr)
+##'
+##' 
+##' @details  Given MIDAS regression:
+##'
+##' \deqn{y_t=\sum_{j=0}^k\sum_{i=0}^{m-1}\theta_{jm+i} x_{(t-j)m-i}+u_t}
+##'
+##' test the null hypothesis that the following restriction holds:
+##'
+##' \deqn{\theta_h=g(h,\lambda),}
+##' where \eqn{h=0,...,(k+1)m}. 
+##' @export
+##' @import numDeriv
+hAh.test <- function(x) {
+
+    prep <- prep_hAh(x)
+    
+    unrestricted <- x$unrestricted
+
+    se2 <- sum(residuals(unrestricted)^2)/(nrow(x$model)-prep$dk)
+    A0 <- (diag(prep$dk)-prep$P%*%tcrossprod(prep$Delta.0,prep$P))/se2        
+    STATISTIC <- t(prep$h.0)%*%A0%*%prep$h.0
+    
+    names(STATISTIC) <- "hAh"
+    METHOD <- "hAh restriction test"
+    PARAMETER <- prep$dk-length(coef(x))
+    PVAL <- 1-pchisq(STATISTIC,PARAMETER)
+    names(PARAMETER) <- "df"
+    
+    structure(list(statistic = STATISTIC, parameter = PARAMETER, 
+        p.value = PVAL, method = METHOD), 
+        class = "htest")
+}
+##' Test restrictions on coefficients of MIDAS regression using robust version of the test
+##'
+##' Perform a test whether the restriction on MIDAS regression coefficients holds.
+##' @param x MIDAS regression model with restricted coefficients, estimated with \code{\link{midas_r}}
+##' @param PHI the "meat" covariance matrix, defaults to \code{vcovHAC(x$unrestricted, sandwich=FALSE)}
+##' @return a \code{htest} object
+##' @author Virmantas Kvedaras, Vaidotas Zemlys
+##' @references Kvedaras V., Zemlys, V. \emph{The statistical content and empirical testing of the MIDAS restrictions}
+##' @seealso hAh.test
+##' @examples
+##'##The parameter function
+##' theta.h0 <- function(p, dk) {
+##'    i <- (1:dk-1)
+##'    (p[1] + p[2]*i)*exp(p[3]*i + p[4]*i^2)
+##' }
+##'
+##' ##Generate coefficients
+##' theta0 <- theta.h0(c(-0.1,0.1,-0.1,-0.001),4*12)
+##'
+##' ##Plot the coefficients
+##' plot(theta0)
+##'
+##' ##Generate the predictor variable
+##' set.seed(13)
+##' x <- simplearma.sim(list(ar=0.6),1500*12,1,12)
+##'
+##' ##Simulate the response variable
+##' y <- midas.sim(500,theta0,x,1)
+##'
+##' ##Remove unnecessary history of x
+##' x <- window(x,start=start(y))
+##' 
+##' ##Fit restricted model
+##' mr <- midas_r(y~fmls(x,4*12-1,12,theta.h0)-1,data.frame(y=y),data.frame(x=x),start=list(x=c(-0.1,0.1,-0.1,-0.001)))
+##' 
+##' ##The gradient function
+##' theta.h0.gradient <-function(p, dk) {
+##'    i <- (1:dk-1)
+##'    a <- exp(p[3]*i + p[4]*i^2)
+##'    cbind(a, a*i, a*i*(p[1]+p[2]*i), a*i^2*(p[1]+p[2]*i))
+##' }
+##'
+##' ##Perform test (the expected result should be the acceptance of null)
+##'
+##' hAhr.test(mr)
+##' 
+##' mr <- midas_r(y~fmls(x,4*12-1,12,theta.h0)-1,data.frame(y=y),data.frame(x=x),start=list(x=c(-0.1,0.1,-0.1,-0.001)),user.gradient=TRUE)
+##'
+##' ##Use exact gradient. Note the 
+##' hAhr.test(mr)
+##' 
+##' @details  Given MIDAS regression:
+##'
+##' \deqn{y_t=\sum_{j=0}^k\sum_{i=0}^{m-1}\theta_{jm+i} x_{(t-j)m-i}+u_t}
+##'
+##' test the null hypothesis that the following restriction holds:
+##'
+##' \deqn{\theta_h=g(h,\lambda),}
+##' where \eqn{h=0,...,(k+1)m}. 
+##' @export
+##' @importFrom MASS ginv
+##' @import sandwich
+hAhr.test <- function(x,PHI=vcovHAC(x$unrestricted,sandwich=FALSE)) {
+    prep <- prep_hAh(x)
+    
+    unrestricted <- x$unrestricted
+
+    nyx <- nrow(x$model)
+    nkx <- ncol(x$model)-1
+    II <- diag(nkx)-prep$XtX %*% prep$Delta.0
+    A0 <- ginv(nyx * ginv(t(prep$P)) %*% II %*% PHI %*% t(II) %*% ginv(prep$P))
+
+    STATISTIC <- t(prep$h.0)%*%A0%*%prep$h.0
+    
+    names(STATISTIC) <- "hAhr"
+    METHOD <- "hAh restriction test (robust version)"
+    PARAMETER <- prep$dk-length(coef(x))
+    PVAL <- 1-pchisq(STATISTIC,PARAMETER)
+    names(PARAMETER) <- "df"
+    
+    structure(list(statistic = STATISTIC, parameter = PARAMETER, 
+        p.value = PVAL, method = METHOD), 
+        class = "htest")
+}
+
+
+##' Calculate data for \link{hAh.test} and \link{hAhr.test}
+##'
+##' Workhorse function for calculating necessary matrices for \link{hAh.test} and \link{hAhr.test}. Takes the same parameters as \link{hAh.test}
+##' @param x \code{midas_r} object
+##' @return a list with necessary matrices
+##' @author Virmantas Kvedaras, Vaidotas Zemlys
+##' @seealso hAh.test, hAhr.test
+prep_hAh <- function(x) {
+
+    unrestricted <- x$unrestricted
+    if(is.null(unrestricted))stop("Unrestricted model cannot be estimated due to the lack of degrees of freedom, testing the restriction is not possible")
+    
+    D0 <- x$gradD(coef(x))
+
+    X <- x$model[,-1]
+    
+    XtX <- crossprod(X)
+
+    dk <- ncol(XtX)    
+
+    if(nrow(D0) != dk)stop("The gradient dimensions are incorrect. Number of rows does not equal number of unrestricted coefficients")
+    
+    P <- chol(XtX)
+
+    cfur <- coef(unrestricted)
+   
+    h.0 <- P%*%(cfur-x$midas.coefficients)
+
+    Delta.0 <- D0%*%tcrossprod(ginv(crossprod(D0,XtX)%*%D0),D0)
+    
+    list(P=P,XtX=XtX,dk=dk,Delta.0=Delta.0,h.0=h.0)
+}
+
+##' Andreou, Ghysels, Kourtellos LM test
+##'
+##' Perform the test whether hyperparameters of normalized exponential Almon lag weights are zero
+##' 
+##' @param x MIDAS regression object of class \code{\link{midas_r}}
+##' @return a \code{htest} object
+##' @author Virmantas Kvedaras, Vaidotas Zemlys
+##' @references Andreou E., Ghysels E., Kourtellos A. \emph{Regression models with mixed sampling frequencies} Journal of Econometrics 158 (2010) 246-261 
+##' @export
+##' @examples
+##' ##' ##Load data
+##' data("USunempr")
+##' data("USrealgdp")
+##'
+##' y <- diff(log(USrealgdp))
+##' x <- window(diff(USunempr),start=1949)
+##' t <- 1:length(y)
+##'
+##' mr <- midas_r(y~t+fmls(x,11,12,nealmon),start=list(x=c(0,0,0)))
+##'
+##' agk.test(mr)
+##'
+agk.test <- function(x) {
+    tl <- attributes(x$terms)$term.labels
+    wn <- grep("nealmon",tl,value=TRUE)   
+    if(length(wn)==0)stop("This test can be only used for regressions with normalized Exponential Almon lag weights")    
+    X <- x$model[,-1]
+    y <- x$model[,1]
+    if(attr(x$terms,"intercept")==1) tl <- c("(Intercept)",tl)
+    Xa <- lapply(tl,function(nm) {
+        if(nm %in% wn) {       
+            apply(X[,grep(wn,colnames(X),fixed=TRUE)],1,mean)
+        }
+        else {
+            X[,nm,drop=FALSE]
+        }
+    })
+    Xa <- do.call("cbind",Xa)
+   
+    ustar <- residuals(lm(y~Xa-1))
+    u <- residuals(x)
+    w <- weight_param(x)
+    if(is.list(w)) {
+        r <- sum(sapply(w,length))
+    }
+    else {
+        r <- length(w)
+    }
+    S.LS <- sum(ustar^2)
+    S.M <- sum(u^2)
+      
+    STATISTIC <- (S.LS-S.M)/S.LS
+    names(STATISTIC) <- "agk"
+    METHOD <- "Andreou, Ghysels, Kourtellos LM test"
+    PARAMETER <- r
+    PVAL <- 1-pchisq(STATISTIC,PARAMETER)
+    names(PARAMETER) <- "df"
+    
+    structure(list(statistic = STATISTIC, parameter = PARAMETER, 
+        p.value = PVAL, method = METHOD), 
+        class = "htest")
+}
