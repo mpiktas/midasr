ba5623ecaa0dc819fced6258e75cd24f73850e78
Vaidotas Zemlys <zemlys@gmail.com>
2013-09-11 16:10:22 +0300

    Add examples for functions in modsel.R

--- R/modsel.R
+++ R/modsel.R
@@ -886,6 +886,18 @@ find_mls_terms <- function(term.name,vars) {
 ##' ## Simulated low-frequency series (e.g. yearly)
 ##' y<-2+0.1*trend+mls(x,0:7,4)%*%fn.x+mls(z,0:16,12)%*%fn.z+rnorm(n)
 ##' ##Do not run
+##' ## cbfc<-select_and_forecast(y~trend+mls(x,0,4)+mls(z,0,12),
+##' ## from=list(x=c(4,8,12),z=c(12,24,36)),
+##' ## to=list(x=rbind(c(14,19),c(18,23),c(22,27)),z=rbind(c(22,27),c(34,39),c(46,51))),
+##' ## insample=1:200,outsample=201:250,
+##' ## weights=list(x=c("nealmon","almonp"),z=c("nealmon","almonp")),
+##' ## wstart=list(nealmon=rep(1,3),almonp=rep(1,3)),
+##' ## IC="AIC",
+##' ## seltype="restricted",
+##' ## ftype="fixed",
+##' ## measures=c("MSE","MAPE","MASE"),
+##' ## fweights=c("EW","BICW","MSFE","DMSFE")
+##' ## )
 ##' 
 select_and_forecast<- function(formula,data,from,to,
                                insample,outsample,
@@ -1034,6 +1046,29 @@ split_data <- function(data,insample,outsample) {
 ##' @return a list containing forecasts and tables of accuracy measures
 ##' @author Virmantas Kvedaras, Vaidotas Zemlys
 ##' @export
+##' @examples
+##' set.seed(1001)  
+##' ## Number of low-frequency observations
+##' n<-250
+##' ## Linear trend and higher-frequency explanatory variables (e.g. quarterly and monthly)
+##' trend<-c(1:n)
+##' x<-rnorm(4*n)
+##' z<-rnorm(12*n)
+##' ## Exponential Almon polynomial constraint-consistent coefficients
+##' fn.x <- nealmon(p=c(1,-0.5),d=8)
+##' fn.z <- nealmon(p=c(2,0.5,-0.1),d=17)
+##' ## Simulated low-frequency series (e.g. yearly)
+##' y<-2+0.1*trend+mls(x,0:7,4)%*%fn.x+mls(z,0:16,12)%*%fn.z+rnorm(n)
+##' mod1 <- midas_r(y ~ trend + mls(x, 4:14, 4, nealmon) + mls(z, 12:22, 12, nealmon),start=list(x=c(10,1,-0.1),z=c(2,-0.1)))
+##' mod2 <- midas_r(y ~ trend + mls(x, 4:20, 4, nealmon) + mls(z, 12:25, 12, nealmon),start=list(x=c(10,1,-0.1),z=c(2,-0.1)))
+##' 
+##' ##Calculate average forecasts
+##' avgf <- average_forecast(list(mod1,mod2),
+##'                         data=list(y=y,x=x,z=z,trend=trend),
+##'                         insample=1:200,outsample=201:250,
+##'                         type="fixed",                            
+##'                         measures=c("MSE","MAPE","MASE"),
+##'                         fweights=c("EW","BICW","MSFE","DMSFE"))
 average_forecast<- function(modlist,data,insample,outsample,type=c("fixed","recursive","rolling"),fweights=c("EW","BICW","MSFE","DMSFE"),measures=c("MSE","MAPE","MASE")) {
 
     #if(length(modlist)==1)stop("Need more than 1 model to produce average forecasts")
