3008eb8936ffcabed45140fe5d4e44104043fa7a
Vaidotas Zemlys <zemlys@gmail.com>
2015-01-02 13:23:03 +0200

    Rewrite the simulation functions

--- R/simulate.R
+++ R/simulate.R
@@ -25,9 +25,9 @@ simplearma.sim <- function(model,n,innov.sd,frequency,n.start=300) {
     ts(res[-n.start:-1],frequency=frequency)
 }
 
-##' Simulate MIDAS regression response variable
+##' Simulate simple MIDAS regression response variable
 ##'
-##' Given the predictor variable and the coefficients calculate MIDAS regression response variable.
+##' Given the predictor variable and the coefficients simulate MIDAS regression response variable.
 ##' 
 ##' @param n The sample size
 ##' @param theta a vector with MIDAS regression coefficients 
@@ -49,48 +49,48 @@ simplearma.sim <- function(model,n,innov.sd,frequency,n.start=300) {
 ##' ##Plot the coefficients
 ##' plot(theta0)
 ##'
-##' ##Generate the predictor variable
-##' x <- simplearma.sim(list(ar=0.6),1500*12,1,12)
+##' ##Generate the predictor variable, leave 4 low frequency lags of data for burn-in.
+##' x <- ts(arima.sim(model = list(ar = 0.6), 504*12), frequency = 12)
 ##'
 ##' ##Simulate the response variable
-##' y <- midas.sim(500,theta0,x,1)
+##' y <- midas_sim(500, x, theta0)
 ##'
-##' @details MIDAS regression has the following form:
+##' x <- window(xx, start=start(y))
+##' midas_r(y ~ mls(y, 1, 1) + fmls(x, 4*12-1, 12, theta.h0), start = list(x = c(-0.1, 10, -10, -10)))
 ##' 
-##' \deqn{y_t=\sum_{j=0}^k\sum_{i=0}^{m-1}\theta_{jm+i} x_{(t-j)m-i}+u_t}
-##'
-##' or alternatively
-##'
-##' \deqn{y_t=\sum_{h=0}^{(k+1)m}\theta_hx_{tm-h}+u_t,}
+##' @details MIDAS regression with one predictor variable has the following form:
+##' 
+##' \deqn{y_t=\sum_{j=0}^{h}\theta_jx_{tm-j}+u_t,}
 ##' where \eqn{m} is the frequency ratio and
-##' \eqn{k} is the number of lags included in the regression. 
+##' \eqn{h} is the number of high frequency lags included in the regression. 
 ##'
 ##' MIDAS regression involves times series with different frequencies. In R
 ##' the frequency property is set when creating time series objects
 ##' \code{\link{ts}}. Hence the frequency ratio \eqn{m} which figures in MIDAS regression is calculated from frequency property of time series objects supplied.
 ##' @export
-midas.sim <- function(n,theta,x,eps.sd) {
+midas_sim <- function(n, x, theta, rand_gen = rnorm, innov = rand_gen(n, ...), ...) {
     m <- frequency(x)
-    n.x <- length(x)
-    
-    if(m==1) stop("The frequency of the predictor variable should be at least 2")
-    if(n.x<=m*n+length(theta)-m) stop("The history of the predictor variable is not long enough, reduce the desired sample size")
+    n_x <- length(x)
+      
+    if(n_x<=m*n+length(theta)-m) stop("The history of the predictor variable is not long enough, reduce the desired sample size")
    
     X <- fmls(x,length(theta)-1,m)
     xt <- as.vector(X%*%theta)
 
-    ts(xt[nrow(X)-n:1+1],start=end(x)[1]-n+1,frequency=1)+rnorm(n,sd=eps.sd)    
+    ts(xt[nrow(X)-n:1+1],start=end(x)[1]-n+1,frequency=1) + innov    
 }
-##' Simulate autoregressive MIDAS model
+##' Simulate simple autoregressive MIDAS model
 ##'
-##' Given the predictor variable, the weights and autoregressive coefficients calculate MIDAS regression response variable.
+##' Given the predictor variable, the weights and autoregressive coefficients, simulate MIDAS regression response variable.
 ##' 
-##' @param n sample size
-##' @param theta a vector with MIDAS weights for predictor variable
-##' @param alpha autoregressive coefficients
-##' @param x a high frequency predictor variable
-##' @param eps.sd the standard error of the regression disturbances, which are assumed to be independent normal zero mean random variables
-##' @param n.start number of observations to omit for the burn.in
+##' @param n sample size.
+##' @param alpha autoregressive coefficients.
+##' @param x a high frequency predictor variable.
+##' @param theta a vector with MIDAS weights for predictor variable.
+##' @param rand_gen a function to generate the innovations, default is the normal distribution.
+##' @param innov an optional time series of innovations.
+##' @param n_start number of observations to omit for the burn.in.
+##' @param ... 
 ##' @return a \code{ts} object
 ##' @author Virmantas Kvedaras, Vaidotas Zemlys
 ##' @export
@@ -105,35 +105,39 @@ midas.sim <- function(n,theta,x,eps.sd) {
 ##' theta0 <- theta.h0(c(-0.1,10,-10,-10),4*12)
 ##'
 ##' ##Generate the predictor variable
-##' xx <- simplearma.sim(list(ar=0.6),3000*12,1,12)
-##'
-##' y <- midas.auto.sim(500,theta0,c(0.5),xx,1,n.start=100)
-##' x <- window(xx,start=start(y))
-##' midas_r(y~mls(y,1,1)+fmls(x,4*12-1,12,theta.h0),start=list(x=c(-0.1,10,-10,-10)))
-midas.auto.sim <- function(n,theta,alpha,x,eps.sd,n.start=NA) {
+##' xx <- ts(arima.sim(model = list(ar = 0.6), 3000 *12), frequency = 12)
+##' 
+##' y <- midas_auto_sim(500, 0.5, xx, theta0, n_start = 200)
+##' x <- window(xx, start=start(y))
+##' midas_r(y ~ mls(y, 1, 1) + fmls(x, 4*12-1, 12, theta.h0), start = list(x = c(-0.1, 10, -10, -10)))
+midas_auto_sim <- function(n, alpha, x, theta, rand_gen = rnorm, innov = rand_gen(n, ...), n_start = NA, ...) {
     m <- frequency(x)
-    n.x <- length(x)
+    n_x <- length(x)
 
     minroots <- min(Mod(polyroot(c(1, -alpha))))
     if (minroots <= 1) 
       stop("'ar' part of model is not stationary")
 
-    if(is.na(n.start))n.start <- 100
+    if(is.na(n_start))n_start <- 100
 
     nout <- n
-    n <- n+n.start
+    n <- n+n_start
                                          
-    if(m==1) stop("The frequency of the predictor variable should be at least 2")
-    if(n.x<=m*n+length(theta)-m) stop("The history of the predictor variable is not long enough, reduce the desired sample size")
+    if(n_x<=m*n+length(theta)-m) stop("The history of the predictor variable is not long enough, reduce the desired sample size")
                                   
 
     X <- fmls(x,length(theta)-1,m)
     xt <- as.vector(X%*%theta)
-    
 
-    xte <- ts(xt[nrow(X)-n:1+1],start=end(x)[1]-n+1,frequency=1)+rnorm(n,sd=eps.sd)
+    ##innov is called here, so its argument n already includes the n_start
+    xte <- ts(xt[nrow(X)-n:1+1],start=end(x)[1]-n+1,frequency=1) + innov
 
     y <- filter(xte,alpha,method="recursive")
-    ts(y[-(1:n.start)],start=end(x)[1]-nout+1,frequency=1)
+    ts(y[-(1:n_start)],start=end(x)[1]-nout+1,frequency=1)
 }
+
+
+##simulate.midas_r
+##1. If there is no autoregression, simply add future xreg times coefficient + bootstrapped error
+##2. If there is autoregression, run dynamic forecast with future xreg, past y and bootstrapped error.
 
