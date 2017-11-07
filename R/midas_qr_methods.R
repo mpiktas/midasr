796e13b208275fc2e8011a9378a68e4de6b3e610
Vaidotas Zemlys <zemlys@gmail.com>
2017-11-07 21:09:23 +0200

    Add summary method for midas_qr. Single tau for the moment.

--- R/midas_qr_methods.R
+++ R/midas_qr_methods.R
@@ -25,3 +25,46 @@ coef.midas_qr <- function(object, midas = FALSE, term_names = NULL, ...) {
  
 }
 
+## Code is borrowed from summary.nlrq in quantreg
+##' @export
+summary.midas_qr <- function(object, ... ) {
+    y <- as.vector(object$residuals)
+    X <- object$model[, -1] %*% object$gradD(coef(object))
+    tau <- object$tau
+    
+    f <- summary(rq(y ~ X - 1, tau), se = "boot", covariance = TRUE, ...)
+    f$coefficients[, 1] <- coef(object)
+    f$coefficients[, 3] <- f$coefficients[, 1]/f$coefficients[, 2]
+    f$coefficients[, 4] <- if (f$rdf > 0) 
+        2 * (1 - pt(abs(f$coef[, 3]), f$rdf))
+    dimnames(f$coefficients)[[1]] <- names(coef(object))
+    f$call <- object$call
+    f$tau <- tau
+    
+    ans <- list(formula=formula(object$terms), residuals=y,
+                call=object$call,
+                f = f,
+                tau = object$tau,
+                coefficients=coef(f), midas_coefficients=coef(object, midas = TRUE),
+                lhs_start = object$lhs_start, lhs_end = object$lhs_end, class_lhs = class(object$lhs))
+    
+    class(ans) <- "summary.midas_qr"
+    ans
+}
+
+##' @export
+print.summary.midas_qr <- function(x, digits =max(3, getOption("digits") - 3 ), 
+                                   signif.stars = getOption("show.signif.stars"), ...)  {
+    
+    cat(paste("\nMIDAS quantile regression model with \"", x$class_lhs[1], 
+              "\" data:\n", sep = ""))
+    cat(paste("Start = ", x$lhs_start, 
+              ", End = ", x$lhs_end, 
+              "\n", sep = ""))
+    cat("\n Formula", deparse(formula(x)),"\n")
+    
+    cat("\n tau:", x$tau, "\n")
+    cat("\n Parameters:\n")
+    printCoefmat(coef(x), digits = digits, signif.stars = signif.stars,...)
+    invisible(x)   
+}
